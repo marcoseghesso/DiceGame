39139c88c6f497c699ef8c11bd97c6ed515b9b56
Marco Seghesso <mseghesso@gmail.com>
2021-12-08 05:45:36 +1300

    Added title displaying result

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const dice1 = document.getElementById("img1");
 const dice2 = document.getElementById("img2");
+const result = document.querySelector("h1");
 
 let randomNum1 = randomNumGenerator(6); 
 let randomNum2 = randomNumGenerator(6);
@@ -8,6 +9,18 @@ function randomNumGenerator(max){
    return Math.floor((Math.random() * max)+1)
 }
 
+function displayResult(num1, num2){
+   if (num1 > num2){
+      result.innerHTML = "Player 1 Won!!"
+   } else if (num1 < num2){
+      result.innerHTML = "Player 2 Won!!"
+   } else {
+      result.innerHTML = "Draw"
+   }
+
+   
+}
+
 dice1.src = `./images/dice${randomNum1}.png`;
 dice2.src = `./images/dice${randomNum2}.png`;
-
+displayResult(randomNum1, randomNum2);
