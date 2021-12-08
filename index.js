0e0c705e8f5eff184065c8cdac05b97432a30c7b
Marco Seghesso <mseghesso@gmail.com>
2021-12-09 12:04:20 +1300

    button added to roll the dice and code refactored

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const dice1 = document.getElementById("img1");
 const dice2 = document.getElementById("img2");
+const roll = document.getElementById("roll");
 const result = document.querySelector("h1");
 
-let randomNum1 = randomNumGenerator(6); 
-let randomNum2 = randomNumGenerator(6);
+let randomNum1; 
+let randomNum2;
 
-function randomNumGenerator(max){
-   return Math.floor((Math.random() * max)+1)
+function randomNumGenerator(){
+   randomNum1 = Math.floor((Math.random() * 6)+1);
+   randomNum2 = Math.floor((Math.random() * 6)+1);
+   updateDice();
+   return
+}
+
+function updateDice(){
+   dice1.src = `./images/dice${randomNum1}.png`;
+   dice2.src = `./images/dice${randomNum2}.png`;
 }
 
 function displayResult(num1, num2){
@@ -17,10 +26,12 @@ function displayResult(num1, num2){
    } else {
       result.innerHTML = "Draw"
    }
-
-   
 }
 
-dice1.src = `./images/dice${randomNum1}.png`;
-dice2.src = `./images/dice${randomNum2}.png`;
-displayResult(randomNum1, randomNum2);
+function rollthedice (){
+   randomNumGenerator();
+   displayResult(randomNum1, randomNum2);
+}
+
+roll.addEventListener("click", rollthedice)
+
